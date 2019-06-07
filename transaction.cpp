52703d52b661862ac0efe7c7582b32dd6617a23d
Krystle S. Levin <41976609+kslevin@users.noreply.github.com>
2019-06-06 17:44:21 -0700

    CODE SKELETON
    .cpp has no implementing code and solely contains comments about functionality of methods

--- transaction.cpp
+++ transaction.cpp
@@ -1,12 +1,71 @@
+/*
+ * Transaction Class Implementation File  @file transaction.cpp
+ *
+ * Transaction is the parent class for different types of transaction
+ *
+ * Transaction main function processCommands is the fall back for when
+ * a transaction could not be processed or invalid. Should this base
+ * function be called the information sent to it will be stored in
+ * error collection.
+ *
+ * @Author: 	Krystle S Levin -kslevin@uw.edu
+ *				Ethan Thomas	-
+ *
+ * @Purpose:	Course Assignment 4 for CSS 343
+ *				i.e. Data Structures, Algorithms and Discrete Mathematics
+ *
+ * @Created on:	June 2, 2019
+ * @last Modification:	June 6, 2019
+ *
+ */
 #include "transaction.h"
 
 
-
+//----------------------------------------------------------------------------
+//------------------------------- Constructor --------------------------------
+//----------------------------------------------------------------------------
 Transaction::Transaction()
 {
+	//TODO: Initialize errorCollection?
+}
+
+//----------------------------------------------------------------------------
+//-------------------------------- Destructor --------------------------------
+//----------------------------------------------------------------------------
+Transaction::~Transaction()
+{
+	//TODO: destruct errorCollection
 }
 
 
-Transaction::~Transaction()
-{
+//----------------------------------------------------------------------------
+//---------------------------- Utility Functions -----------------------------
+//----------------------------------------------------------------------------
+/*****************************************************************************
+ * Function: processCommands
+ * @Param : 		 commandFile where the command char has already be read
+ * @Post-condition:
+*****************************************************************************/
+void Transaction::processCommands(ifstream& commandFile) {
+	/*	
+	 *
+	 *				  TODO: ERROR CATCHING
+	 *	 If commands are being processed here it means 
+	 *	that it is an invalid instruction: add to errors
+	 * 
+	 *  errorCollection.push_back( getline()??);  //still working on syntax
+	 *
+	 */
+}
+
+/*****************************************************************************
+ * Function: printErrors
+ * @Param : 		
+ * @Post-condition:
+*****************************************************************************/
+void Transaction::printErrors() {
+
+	// traverse error collection
+	// NOTE: newest Transaction Last 
+
 }
--- transaction.h
+++ transaction.h
@@ -1,7 +1,27 @@
+/*
+ * Transaction Class Header File  @file transaction.h
+ *
+ * Transaction is the parent class for different types of transaction
+ *
+ * Transaction main function processCommands is the fall back for when
+ * a transaction could not be processed or invalid. Should this base
+ * function be called the information sent to it will be stored in 
+ * error collection.
+ *
+ * @Author: 	Krystle S Levin -kslevin@uw.edu
+ *				Ethan Thomas	- 
+ *
+ * @Purpose:	Course Assignment 4 for CSS 343
+ *				i.e. Data Structures, Algorithms and Discrete Mathematics
+ *
+ * @Created on:	June 2, 2019
+ * @last Modification:	June 6, 2019
+ *
+ */
 #pragma once
 #include <iostream>
 #include <vector> 
-#include "inventoryDatabase.h"
+#include "customer.h"
 #include "movie.h"
 
 using namespace std;
@@ -13,9 +33,10 @@ public:
 	virtual ~Transaction() {};  // destructor
 
 // processes commands
-	virtual void processCommands(CustomerDatabase&, InventoryDatabase&);
+	virtual void processCommands(ifstream&);
 	void printErrors();                              // prints error massages
 	static const char DVD = 'D';                 // shared by all transactions
+
 protected:
 
 	vector <string> errorCollection;   //vector that holds all type of errors during                reading of the commands
