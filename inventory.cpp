03f0c4e7974d7be9f6b9cdaf1698f8ad8a3d11e3
Krystle S. Levin <41976609+kslevin@users.noreply.github.com>
2019-06-06 22:22:22 -0700

    Done?

--- inventory.cpp
+++ inventory.cpp
@@ -1,6 +1,21 @@
+/*
+ * Inventory Class Implementation File  @file inventory.cpp
+ *
+ * Inventory is inherited from Transaction
+ *
+ * Inventory prints out the entire Inventory of the store.
+ *
+ * @Author: 	Krystle S Levin -kslevin@uw.edu
+ *				Ethan Thomas	-
+ *
+ * @Purpose:	Course Assignment 4 for CSS 343
+ *				i.e. Data Structures, Algorithms and Discrete Mathematics
+ *
+ * @Created on:	June 2, 2019
+ * @last Modification:	June 6, 2019
+ *
+ */
 #include "inventory.h"
-
-
 
 Inventory::Inventory()
 {
@@ -10,6 +25,14 @@ Inventory::Inventory()
 Inventory::~Inventory()
 {
 }
-void Inventory::processCommands(ifstream& commandFile) {
+/*****************************************************************************
+ * Function:	processCommands
+ * @Param : 		 The command type has already been extracted from the file
+ *					 There is no more data in the rest of the file line
+ *
+ * @Post-condition:  The entire store inventory will be printed.
+*****************************************************************************/
+void Inventory::processCommands(char type, ifstream& commandFile) {
+	theShelves.displayAll();
 
 }
--- inventory.h
+++ inventory.h
@@ -1,3 +1,20 @@
+/*
+ * Inventory Class header File  @file inventory.h
+ *
+ * Inventory is inherited from Transaction
+ *
+ * Inventory prints out the entire Inventory of the store.
+ *
+ * @Author: 	Krystle S Levin -kslevin@uw.edu
+ *				Ethan Thomas	-
+ *
+ * @Purpose:	Course Assignment 4 for CSS 343
+ *				i.e. Data Structures, Algorithms and Discrete Mathematics
+ *
+ * @Created on:	June 2, 2019
+ * @last Modification:	June 6, 2019
+ *
+ */
 #pragma once
 #include "transaction.h"
 
@@ -8,7 +25,7 @@ public:
 	Inventory();                 // constructor
 	~Inventory();                // destructor
 	static const char MyType = 'I';           // static identifier for the class
-	virtual void processCommands(ifstream& commandFile); // process Transaction
+	virtual void processCommands(char, ifstream& commandFile); // process Transaction
 };
 
 
