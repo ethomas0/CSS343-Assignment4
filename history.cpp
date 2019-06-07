c0c5c5b9a3eca79b001b56e26b653ad4c1ebe4d4
Krystle S. Levin <41976609+kslevin@users.noreply.github.com>
2019-06-06 22:15:50 -0700

    Done.
    No additional functions needed

--- history.cpp
+++ history.cpp
@@ -1,6 +1,21 @@
+/*
+ * History Class implentation File  @file History.cpp
+ *
+ * History is inherited from Transaction
+ *
+ * History prints out the history of a specified customer.
+ *
+ * @Author: 	Krystle S Levin -kslevin@uw.edu
+ *				Ethan Thomas	-
+ *
+ * @Purpose:	Course Assignment 4 for CSS 343
+ *				i.e. Data Structures, Algorithms and Discrete Mathematics
+ *
+ * @Created on:	June 2, 2019
+ * @last Modification:	June 6, 2019
+ *
+ */
 #include "history.h"
-
-
 
 History::History()
 {
@@ -9,4 +24,24 @@ History::History()
 
 History::~History()
 {
+}
+/*****************************************************************************
+ * Function: Borrow:: processCommands
+ * @Param : 		 The command type has already been extracted from the file
+ *					 the rest of the Command file line contains only the 
+ *					 customerId, which is extracted as type int.
+ *
+ * @Post-condition:  If the customer exists in the system their history is
+ *					 printed, if not an error is sent to error collection
+*****************************************************************************/
+void History::processCommands(char type, ifstream& commandFile) {
+	int customerID;
+	commandFile >> customerID;
+
+	if(transactee.getCustomer(customerID) == NULL ){
+		errorCollection.push_back("Sorry, Customer ID: "+to_string(customerID)+" does not exist");
+	}
+	else {
+		transactee.getCustomer(customerID)->printHistory();
+	}
 }
--- history.h
+++ history.h
@@ -1,3 +1,20 @@
+/*
+ * History Class Header File  @file History.h
+ *
+ * History is inherited from Transaction
+ *
+ * History prints out the history of a specified customer.
+ *
+ * @Author: 	Krystle S Levin -kslevin@uw.edu
+ *				Ethan Thomas	-
+ *
+ * @Purpose:	Course Assignment 4 for CSS 343
+ *				i.e. Data Structures, Algorithms and Discrete Mathematics
+ *
+ * @Created on:	June 2, 2019
+ * @last Modification:	June 6, 2019
+ *
+ */
 #pragma once
 #include "transaction.h"
 
@@ -8,7 +25,7 @@ public:
 	History();                 // constructor
 	~History();                // destructor
 	static const char MyType = 'H';           // static identifier for the class
-	virtual void processCommands(CustomerDatabase&, InventoryDatabase&); // process Transaction
+	virtual void processCommands(char, ifstream&); // process Transaction
 };
 
 
