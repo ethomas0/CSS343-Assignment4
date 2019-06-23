84c84bac19da8f76a17548fbb55c00897f94dcb7
ethomas0 <51215431+ethomas0@users.noreply.github.com>
2019-06-23 14:16:31 -0700

    Update customer.h

--- customer.h
+++ customer.h
@@ -1,43 +1,63 @@
-#pragma once
-#include<iostream>
+//------------------------------ customer.h ---------------------------------
+// --Krystal Levin & Ethan Thomas
+//---------------------------------------------------------------------------
+
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+#include <fstream> 
+#include <iostream>
+#include <string> 
 #include "movie.h"
-
-
 using namespace std;
+
+//--------------------------  class Customer  -------------------------------
+// Purpose: 
+//	 -- Customer class contains ID, first, and last name. Also,
+//      customer keeps track of their borrowed and returned movies.
+//
+// Implementation and assumptions:
+//   -- Implement with  a linked list 
+//   -- Implement with following futures: 
+//      newTransaciton, printHistory, getIDNum, getName, getfirstName,
+//		getLastName, checkIfBorrowed.
+//---------------------------------------------------------------------------
 
 class Customer
 {
 public:
 
-	Customer();			     // default constructor
-	Customer(int, string, string); // constructor
-	~Customer();                    // destructor
-	bool movieBorrow(Movie*);       // borrow movie
-	bool movieReturn(Movie*);       // return movie
+	Customer();							// default constructor
+	Customer(int, string, string);		// constructor with parameters
+	~Customer();						// destructor
+	bool newTransaction(char, Movie*);  // adds transaction to customer history
 
-	void printHistory();           // print customer borrow and return history
-	int getIDNum() const;          // return customer ID
+	void printHistory() const;				// print customer borrow and return history
+	int getIDNum()const;				// return customer ID
+	string getID()const;						// get customer ID
+	string getName()const;				// returns string of customer full name
+	string getFirstName()const;			// returns the string of the customer first name
+	string getLastName()const;			// returns the string of the custome last name
+	bool checkIfBorrowed(Movie*)const; // checks if customer is returning a movie they borrowed
 
-private:
-
-	bool returnMovie(Movie*);      // returns one or many movie(s) for customer (if possible)
-	bool borrowMovie(Movie*);      // borrows one or many movie(s) for customer (if possible) 
-	int ID;               		// customer id
-	string lastName;               // customer last name
-	string firstName;              // customer first name
-
+private: 
+	
+	int ID;               				// customer id
+	string lastName;					// customer last name
+	string firstName;					// customer first name
 	
 
-
-	// structure for customer transaction history
+// structure for customer transaction history
 	struct  TransactionLog
 	{
-		TransactionLog* next;
-		char transaction;   // transaction type (Borrow or Return)
-		Movie* info;       // pointer to the movie
+		TransactionLog* next;        // next
+		char transaction;			// transaction type (B or R)
+		Movie* info;				// pointer to the movie
+		bool rented;
+		TransactionLog(char type, Movie* movie) : transaction(type), info(movie), next(nullptr), rented(false) {};
 	};
 	TransactionLog* head;
+	
 
 };
-
+#endif
 
