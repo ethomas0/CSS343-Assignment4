d5412461a3b2f12e118e2bd65dbe77a521381487
ethomas0 <51215431+ethomas0@users.noreply.github.com>
2019-06-23 14:13:49 -0700

    Update classics.h

--- classics.h
+++ classics.h
@@ -1,23 +1,52 @@
-#pragma once
+//---------------------------- classics.h -----------------------------------
+// --Krystal Levin & Ethan Thomas
+//---------------------------------------------------------------------------
+
+#ifndef CLASSICS_H
+#define CLASSICS_H
+#include <string>
 #include "movie.h"
+using namespace std;
+//--------------------------  class Classics --------------------------------
+// Purpose:
+//   -- child class of Movie. Classical movie has extra info:
+//      month and majorActor. Also, classical movie sort by majoractor, month,
+//      and year.
+//
+// Implementation and assumptions:
+//   -- Implement as a child class of Movie
+//   -- Implement with following futures:
+//      createMovieInfo, createCustomerHistory,
+//		==, !=, >, <, and isEqual 
+//   -- Assumption: each Classics movie that has same director, same
+//      title, same year and month, but has different Major will be 
+//      considirate as a different Classic movie.
+//----------------------------------------------------------------------------
 class Classics : public Movie
 {
 public:
 
-	Classics(int stock, string director, string title, string majorActor, int month, int year);  // constructor
-	~Classics();                                // destructor
+	~Classics();										// destructor
+	Classics(const string&);                            // constructor
+	Classics(const string&, const int&, const int&);    // constructor
 
-	bool operator==(const Movie* rhs) const = 0;   // equal comparison operator
-	bool operator!=(const Movie* rhs) const = 0; // not equal comparison operator
-	bool operator>(const Movie* rhs) const = 0; // greater than operator
-	bool operator<(const Movie* rh) const = 0;  // less than operator 
+	virtual string createMovieInfo() const;            // returns string of formated movie info
+	virtual string createCustomerHistory() const;    // returns string of movie info for customer transactions
+	//string getMajorActor()const;
 
-	void printMovieInfo() const;              // return string of full movie  info 
+	static const char GENRE = 'C';                   // static identifier for the class
+	bool isEqual(const Movie* rhs)const;		// used for alterante classic movie with same title, month, and year, but different title
 
-	static const char CODE = 'C';                   // static identifier for the class
+
 protected:
-	int month;
-	string majorActor;
+
+	void parseNLoadString(const string& movieInfo);     // helper method for constractor
+	bool operator==(const Movie* rhs) const;			// equal comparison operator
+	bool operator!=(const Movie* rhs) const;			// not equal comparison operator
+	bool operator>(const Movie* rhs) const;				// greater than operator
+	bool operator<(const Movie* rh) const;				// less than operator 
+
+	int month;                                          // hold month                                
+	string majorActor;                                  // hold major actor
 };
-
-
+#endif
