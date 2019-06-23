ac52696719d843dd8d31785b07957b5050c93de3
ethomas0 <51215431+ethomas0@users.noreply.github.com>
2019-06-23 14:27:04 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,39 @@
-
-#include<iostream>
-#include <fstream> 
-#include "store.h" 
-
-
+#include "storemanager.h"
+#include <iostream>
 using namespace std;
+
+//// Used for memory leak detection
+//#if defined(_WIN32) || defined(_WIN64)
+//#include "vld.h"
+//#endif
 
 int main()
 {
-	ifstream infile1("data4commands.txt");
+	ifstream infile1("data4movies.txt");
 	if (!infile1) {
 		cout << "File could not be opened." << endl;
 		return 1;
 	}
-	ifstream infile2("data4customers.txt");
+	ifstream infile2("data4commands.txt");
 	if (!infile2) {
 		cout << "File could not be opened." << endl;
 		return 1;
 	}
-	ifstream infile3("data4movies.txt");
+
+	ifstream infile3("data4customers.txt");
 	if (!infile3) {
 		cout << "File could not be opened." << endl;
 		return 1;
 	}
 
-	Store firstStore;
-	firstStore.buildStore(infile2, infile3);
-	firstStore.processCommands(infile1); 
+	StoreManager store;                   // allocate store
+	store.buildStore(infile1, infile3);   // proccess movies and customers
+	store.readCommands(infile2);        // proccess commanads
 
+	infile1.close();
+	infile2.close();
+	infile3.close();
+
+	//system("pause");
 	return 0;
 }
