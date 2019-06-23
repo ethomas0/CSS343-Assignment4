61be2bcb1bb92169c68d1ffa5f77dcfdf008d953
ethomas0 <51215431+ethomas0@users.noreply.github.com>
2019-06-23 14:30:46 -0700

    Update and rename store.cpp to storemanager.cpp

---
+++ storemanager.cpp
@@ -0,0 +1,36 @@
+//----------------------------- storemanager.cpp ----------------------------
+// --Krystal Levin & Ethan Thomas
+//---------------------------------------------------------------------------
+
+#include <string>
+#include "string.h"
+#include <iostream>
+#include <fstream>
+#include "storemanager.h"
+
+//--------------------------  class StoreManager ----------------------------
+// Purpose:
+//   -- Store Manager class: high level manager for other manager
+//      classes such as: CustomerManager, InventoryManager, ComandLoaderManager.
+//
+// Implementation and assumptions:
+//   -- Implement as a manager class
+//   -- Implement with following futures:
+//      buildStore, readCommands
+//---------------------------------------------------------------------------
+
+//----------------------------- buildStore ----------------------------------
+// --Processes inventory file(movieFile) and customer file(customerFile)
+//---------------------------------------------------------------------------
+void StoreManager::buildStore(ifstream& movieFile, ifstream& customerFile)
+{
+	allInventory.processInventory(movieFile);
+	allCustomers.proccessCustomers(customerFile);
+}
+
+//------------------------------ readCommands -------------------------------
+// --Processes transaction commmands file (commands)
+//---------------------------------------------------------------------------
+void StoreManager::readCommands(ifstream& commands) {
+	allTransactions.readCommands(commands, allCustomers, allInventory);
+}
