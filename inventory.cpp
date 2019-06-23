7a1fab9387c5b5972114cf6f7630ce10c3c314b3
ethomas0 <51215431+ethomas0@users.noreply.github.com>
2019-06-23 14:24:32 -0700

    Update inventory.cpp

--- inventory.cpp
+++ inventory.cpp
@@ -1,12 +1,32 @@
+//------------------------------- inventory.cpp ------------------------------
+// --Krystal Levin & Ethan Thomas
+//----------------------------------------------------------------------------
+
 #include "inventory.h"
 
+//----------------------------- class Inventory ------------------------------
+// --Purpose:
+//	  -- Inventory is a class for viewing the store's Inventory and is 
+//		 inherited from Transaction
+//----------------------------------------------------------------------------
 
 
-Inventory::Inventory()
+//------------------------------- Constructor --------------------------------
+//
+// --Param:
+//	  --commandLine is a string containing the transaction code 'I'
+// --Post-condition:
+//	  --Calls Inventory Manager to execute the display all function.
+//----------------------------------------------------------------------------
+ Inventory::Inventory(const string& commandLine, CustomerManager& theCustomers, InventoryManager& theInventory)
 {
+	theInventory.displayAll(); // ask inventory manager to display all
 }
 
-
+//----------------------------- Destructor -----------------------------------
+//
+//----------------------------------------------------------------------------
 Inventory::~Inventory()
 {
+	//empty on purpose
 }
