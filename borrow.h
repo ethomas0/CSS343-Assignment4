c00f97d2047483676ae25ad6e0f46410dddeadea
ethomas0 <51215431+ethomas0@users.noreply.github.com>
2019-06-23 14:11:51 -0700

    Update borrow.h

--- borrow.h
+++ borrow.h
@@ -1,18 +1,29 @@
-#pragma once
-#include "transaction.h"
-#include "customerDatabase.h"
+//----------------------------borrow.h---------------------------------------
+// --Krystal Levin & Ethan Thomas
+//---------------------------------------------------------------------------
 
+#ifndef BORROW_H
+#define BORROW_H
+#include "transaction.h"
+
+//--------------------------  class Borrow ----------------------------------
+// Purpose: 
+//	 -- Child class of Transaction. Borrows movie for customer
+//		and updates the InventoryManager
+//
+// Implementation and assumptions:
+//   -- Implement as a child class of Transaction
+//   -- Implement with following futures:
+//      Borrow (as Borrow is constructed it also creates the transaction or
+//		error code depending on situation)
+//---------------------------------------------------------------------------
 class Borrow : public Transaction
 {
 public:
+	Borrow(const string&, CustomerManager&, InventoryManager&);	// constructor
+	~Borrow();												// destructor
 
-	Borrow();                 // constructor
-	~Borrow();                // destructor
-	static const char MyType = 'B';           // static identifier for the class
-	virtual void processCommands(CustomerDatabase&, InventoryDatabase&); // process Transaction
-protected:
-
-	void borrowMovie(Customer*, Movie*);   // borrowed movie for customer
+	static const char CODE = 'B';							// Transaction Code
 };
-
+#endif
 
