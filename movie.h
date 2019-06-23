b041c204cb294ada7e4ba372d394370b9ac145b4
ethomas0 <51215431+ethomas0@users.noreply.github.com>
2019-06-23 14:27:59 -0700

    Update movie.h

--- movie.h
+++ movie.h
@@ -1,35 +1,62 @@
-#pragma once
-#include <iostream> 
+//--------------------------------- movie.h ----------------------------------
+// --Krystal Levin & Ethan Thomas
+//----------------------------------------------------------------------------
+#ifndef MOVIE_H
+#define MOVIE_H
 
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
 using namespace std;
 
-class Movie
+//--------------------------  class Movie -----------------------------------
+// Purpose:
+//   -- abstract class Movie. Abstact class for child classes Drama,
+//      Comdedy, Classiscs. Can not be created. Used only for inheretance purpose.
+//   -- General movie contains information about title, director, year, and stock.
+//
+// Implementation and assumptions:
+//   -- Implement as a abstract class
+//   -- Implement with following futures:
+//      createMovieInfo(), ==, !=, >, <
+//---------------------------------------------------------------------------
+class Movie 
 {
 public:
-
 	virtual ~Movie() {};                // destructor
+	
+	virtual string createMovieInfo() const = 0;               //  pure virtual printInfo
+	virtual string createCustomerHistory() const = 0;    // pure virtual printInfoForCustomer
 
-	virtual bool operator==(const Movie* rhs) const = 0;   // == operator
-	virtual bool operator!=(const Movie* rhs) const = 0; // != operator
-	virtual bool operator>(const Movie* rhs) const = 0; // greater than operator
-	virtual bool operator<(const Movie* rhs) const = 0;  // less than operator
+	virtual bool increaseStock();    // increase stock
+	virtual bool decreaseStock();       // decrease stock
+	virtual int getStock();             // return current stock
 
-	virtual void printMovieInfo() const = 0;      //  prints movies information
 
-	virtual bool increaseStock(const int& amount) = 0;    // increase stock
-	virtual bool dicreaseStock(const int& amount) = 0;    // decrease stock
-	virtual int getStock();                      // return current stock
+
+	
+	virtual bool operator==(const Movie* rhs) const =0; // pure virtual isEqual operator
+	virtual bool operator!=(const Movie* rhs) const =0;
+	virtual bool operator>(const Movie* rhs) const =0;    // operator=>
+	virtual bool operator<(const Movie* rhs) const =0; 
+	
+	
+	
 
 protected:
 
-	explicit Movie();                            // constructor
+	Movie();                            // constructor (protected) to avoid declaration of the object
+	void parseNLoadString(const string& movieInfo);     // helper method for copy constractor
 
 
-	char type;                          // holds a movie type
-	int stock;                          // holds movie stock
-	string director;                    // holds a movie director
-	string title;                       // holds a movie title
-	int year;                           // holds a movie year 
+	//movie data
+	char type;                          // hold type
+	int stock;                          // hold stock
+	string director;                    // hold director
+	string title;                       // hold title
+	int year;                           // hold year
+
 };
-
-
+#endif
